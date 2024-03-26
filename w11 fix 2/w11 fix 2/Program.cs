ccff67a0968722f1e254e48c51e6c94a9d49c1f0
AngelApriliaPutri <164986194+AngelApriliaPutri@users.noreply.github.com>
2024-03-26 12:41:10 +0700

    Add files via upload

---
+++ w11 fix 2/w11 fix 2/Program.cs
@@ -0,0 +1,76 @@
+﻿internal class Program
+{
+    static void win()
+    {
+        Console.WriteLine("YOU WIN!!");
+    }
+    static void lose()
+    {
+        Console.WriteLine("YOU LOSE!!");
+    }
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Input kata : ");
+        string input = Console.ReadLine();
+        List<char> guess = new List<char> (input.ToList<char>());
+        char[] hastag = new char[100];
+        int nyawa = 3;
+        int p = 0;
+        int selesai = 0;
+        int b = 0;
+        Console.Clear();
+        Console.WriteLine("Guess the word!");
+        Console.WriteLine("Nyawa : ♥♥♥");
+        for (int i = 0; i < input.Length; i++)
+        {
+            hastag[i] = '#';
+            Console.Write(hastag[i]);
+        }
+        
+        while (p == 0 )
+        {
+            Console.WriteLine();
+            b = 0;
+            Console.WriteLine("Guess : ");
+            char guess2 = Convert.ToChar(Console.ReadLine());
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (guess2 == input[i])
+                {
+                    hastag[i] = Convert.ToChar(Convert.ToString(guess2).ToUpper());
+                    selesai = selesai + 1;
+                    b= b - 1; 
+                }
+            }
+            if ((b >= 0) && (b==0))
+            {
+                Console.WriteLine(Convert.ToString(guess2).ToUpper() + " Tidak ditemukan");
+                nyawa = nyawa - 1;
+                Console.Write("Nyawa = ");
+                    for (int k = 0; k < nyawa; k++)
+                    {
+                        Console.Write('♥');
+                    }
+            }
+            else if (b < 0)
+            {
+                Console.WriteLine(Convert.ToString(guess2).ToUpper() + " Ditemukan");
+                b = 0;
+            }
+            Console.WriteLine();
+            Console.WriteLine(hastag);
+            if (selesai == input.Length)
+            {
+                p = 1 ;
+                Console.WriteLine("The Word is " + input.ToUpper());
+                win();
+            }
+            else if (nyawa == 0)
+            {
+                lose();
+                p = 1;
+            }
+
+        }
+    }
+}
